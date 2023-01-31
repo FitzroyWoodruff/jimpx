be60e3fbb75c9cf36ac04ba9f98b83590091e72b
Fitzroy Woodruff <fitzroywoodruff@gmail.com>
2023-01-30 21:46:28 -0500

    tested image duplicate

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Jimp = require("jimp");
 	//read the image
 	const image = await Jimp.read("img/bbb.jpg");
 
+	const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+	image.print(font, 0, 0, "This is a test");
 	//save the image
-	image.write("out/newImage.png");
+	image.write("out/newImage2.png");
 })();
